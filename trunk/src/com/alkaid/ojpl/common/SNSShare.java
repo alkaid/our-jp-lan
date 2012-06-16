a642cbf02a2a1b9bdbc8df269fe5a48be028fe54
ancoodroid@gmail.com <ancoodroid@gmail.com@797d2ecb-b29e-d9b5-5e7c-d46a2d0f8d72>
2012-06-16 06:35:35 +0000

    sns分享奖励积分

--- trunk/src/com/alkaid/ojpl/common/SNSShare.java
+++ trunk/src/com/alkaid/ojpl/common/SNSShare.java
@@ -2,10 +2,15 @@ package com.alkaid.ojpl.common;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.HashMap;
 import android.content.Context;
+import android.content.SharedPreferences;
 import android.util.Log;
 import android.widget.Toast;
+
+import com.alkaid.ojpl.view.ad.PointsManager;
 import com.umeng.api.exp.UMSNSException;
 import com.umeng.api.sns.UMSnsService;
 import com.umeng.api.sns.UMSnsService.DataSendCallbackListener;
@@ -32,6 +37,7 @@ public class SNSShare {
     		public void onDataSendFailedWithException(
     		UMSNSException exception,
     		UMSnsService.SHARE_TO userPlatform) {
+    			Toast.makeText(context, "发布失败", Toast.LENGTH_SHORT).show();
     		}
     		@Override
     		public void onDataSendFinished(RETURN_STATUS
@@ -39,10 +45,18 @@ public class SNSShare {
     		switch (returnStatus)
     		{
     		case UPDATED:
-        		Log.i("Log", "Success!");
+    			Toast.makeText(context, "发布成功", Toast.LENGTH_LONG).show();
+    			//积分奖励机制
+    			String date = getSharedDate(context, userPlatform);
+				String now=new SimpleDateFormat("yy-MM-dd").format(new Date());
+				if(!now.equals(date)){
+					PointsManager pointsManager=new PointsManager(context);
+					pointsManager.awardPoints(Constants.points.awardPerShare);
+					writeSharedDate(context, userPlatform);
+				}
         		break;
     		case REPEATED:
-        		Log.i("Log", "Repeated!");
+        		Toast.makeText(context, "不能发布重复内容", Toast.LENGTH_SHORT).show();
         		break;
     		case SEND_TIME_EXTENDS_LIMIT:
     			Toast.makeText(context, SHARENETERROR, Toast.LENGTH_SHORT);
@@ -63,5 +77,44 @@ public class SNSShare {
 			LogUtil.e(e);
 		}
     	
+	}
+	
+	private static void writeSharedDate(Context context,UMSnsService.SHARE_TO userPlatform){
+		SharedPreferences sp=context.getSharedPreferences(Constants.sharedPreference.snsShare.name, Context.MODE_PRIVATE);
+		String key=null;
+		switch (userPlatform) {
+		case SINA:
+			key=Constants.sharedPreference.snsShare.sinaDate;
+			break;
+		case RENR:
+			key=Constants.sharedPreference.snsShare.renrenDate;
+			break;
+		case TENC:
+			key=Constants.sharedPreference.snsShare.qqweiboDate;
+			break;
+		default:
+			break;
+		}
+		String date=new SimpleDateFormat("yy-MM-dd").format(new Date());
+		sp.edit().putString(key, date).commit(); 
+	}
+	
+	private static String getSharedDate(Context context,UMSnsService.SHARE_TO userPlatform){
+		SharedPreferences sp=context.getSharedPreferences(Constants.sharedPreference.snsShare.name, Context.MODE_PRIVATE);
+		String key=null;
+		switch (userPlatform) {
+		case SINA:
+			key=Constants.sharedPreference.snsShare.sinaDate;
+			break;
+		case RENR:
+			key=Constants.sharedPreference.snsShare.renrenDate;
+			break;
+		case TENC:
+			key=Constants.sharedPreference.snsShare.qqweiboDate;
+			break;
+		default:
+			break;
+		}
+		return sp.getString(key, null);
 	}
 }
